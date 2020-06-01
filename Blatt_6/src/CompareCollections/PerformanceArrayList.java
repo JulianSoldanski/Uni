1fa4dd3d969e2833c26b69a5b4c5733ce8157595
Julian Soldanski <JulianSoldanski@gmx.de>
2020-06-01 18:18:27 +0200

    Missing Table

--- Blatt_6/src/CompareCollections/PerformanceArrayList.java
+++ Blatt_6/src/CompareCollections/PerformanceArrayList.java
@@ -11,20 +11,28 @@ public class PerformanceArrayList {
 		Collection <Integer> linkedCollection = new LinkedList<Integer>();
 		Collection <Integer> hashCollection = new HashSet<Integer>();
 		
+		Collection[] toTest = new Collection[] {arrayCollection,linkedCollection,hashCollection};
 		
-		for(Collection<Integer> a : hashCollection) {
-			ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		for(Collection <Integer> a : toTest){
 			long start = System.nanoTime();
 			for(int i = 0; i < 100000; i++) {
-				arrayList.add(5);
+				a.add(i);
 			}
 			long add = System.nanoTime();
+			
 			for(int i = 0; i < 100000; i++) {
-				arrayList.contains(i);
+				a.contains(i);
 			}
 			long contains = System.nanoTime();
-			System.out.println("Time add in nanosec: " + ((add-start)/ 100000));
-			System.out.println("Time contains in nanosec: " + ((contains-add)/100000));
+			
+			for(int i = 0; i < 100000; i++) {
+				a.remove(i);
+			}
+			long remove = System.nanoTime();
+			
+			System.out.println( a.getClass() +" Time: 'add()' (nanosec) : " + ((add - start)/100000));
+			System.out.println( a.getClass() +" Time: 'contains()' (nanosec) : " + ((contains-add)/100000));
+			System.out.println( a.getClass() +" Time: 'delete()' (nanosec) : " + ((remove-contains)/100000));
 				
 		}
 	}
